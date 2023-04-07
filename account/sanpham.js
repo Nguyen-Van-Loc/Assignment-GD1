fe78ee3a8cf8be5e15ea3c47bc73842e6864a6f1
Lộc Văn <loccosi3005@gmail.com>
2023-04-07 08:50:38 +0700

    a

--- account/sanpham.js
+++ account/sanpham.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1/Taikhoan');
 const Schema = mongoose.Schema;
 const sanpham = new Schema({
-    tensp: { type: String, require: true },
-    gia: { type: Number, require: true },
-    mota: { type: String },
-    image: { type: String }
+    tensp: String,
+    gia:  Number,
+    mota:  String,
+    image: String
 }, { collection: 'sanpham' });
 const SanphamModel = mongoose.model('sanpham', sanpham)
 module.exports = SanphamModel
--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var AccountModel1 = require('./account/accont')
+var SanphamModel1 = require('./account/sanpham')
 var router = express.Router()
 const {mutipleMongooesToObject}=require('./util/monggo')
 router.get('/', (req, res) => {
@@ -16,6 +17,11 @@ router.get('/dangki', (req, res) => {
 router.get('/trangchu', (req, res, next) => {
     AccountModel1.find({}).then(account => {
         res.render('trangchu', {account:mutipleMongooesToObject(account)})
+    }).catch(next);
+})
+router.get('/sanpham', (req, res, next) => {
+    SanphamModel1.find({}).then(sanpham => {
+        res.render('sanpham', {sanpham:mutipleMongooesToObject(sanpham)})
     }).catch(next);
 })
 module.exports = router;
